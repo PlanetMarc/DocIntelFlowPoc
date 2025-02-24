5a77938492dc50263f6b83958a551474afe9a87e
Marc Merritt <mmerritt@go-planet.com>
2025-02-24 16:27:43 -0500

    Tokenized and can load files from local

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿﻿﻿﻿﻿﻿using System;
+﻿﻿﻿﻿﻿﻿﻿﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,7 +11,7 @@ namespace PlanetTech.AI.DocIntelFlowPoc
 {
     class Program
     {
-        static string key = "205a573dee714306b532d1cadb3e86f4";
+        static string key = "";
         static string endpoint = "https://demo-ak-dot.cognitiveservices.azure.com/";
         static AzureKeyCredential _credential = new AzureKeyCredential(key);
         static DocumentIntelligenceClient _docIntelClient = new DocumentIntelligenceClient(new Uri(endpoint), _credential);
@@ -64,11 +64,38 @@ namespace PlanetTech.AI.DocIntelFlowPoc
 
         public static async Task AnalyzeDocumentAsync(string document)
         {
-            Uri fileUri = new Uri("https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/rest-api/read.png");
-            Operation<AnalyzeResult> operation = await _docIntelClient.AnalyzeDocumentAsync(WaitUntil.Completed, "prebuilt-read", fileUri);
+            //Uri fileUri = new Uri("https://github.com/PlanetMarc/DocIntelFlowPoc/blob/main/docs/sample1.TIF");
+            //Operation<AnalyzeResult> operation = await _docIntelClient.AnalyzeDocumentAsync(WaitUntil.Completed, "prebuilt-read", fileUri);
             
-            //Operation<AnalyzeResult> operation = await _docIntelClient.AnalyzeDocumentAsync(WaitUntil.Completed, "prebuilt-read", BinaryData.FromString(document));
+            var docFileBytes = File.ReadAllBytes(document);
+            var cts = new CancellationTokenSource();
+
+            // Give the user a status update as the operation completes
+            var spinnerTask = Task.Run(async () =>
+            {
+                while (!cts.Token.IsCancellationRequested)
+                {
+                    Console.Write(".");
+                    await Task.Delay(100);
+                }
+            });
+
+            Operation<AnalyzeResult> operation = await _docIntelClient.AnalyzeDocumentAsync(WaitUntil.Completed, "prebuilt-read", BinaryData.FromBytes(docFileBytes));
+            cts.Cancel();
+            await spinnerTask;
+
             AnalyzeResult result = operation.Value;
+            var content = result.Content;
+
+            Console.WriteLine("");
+            Console.WriteLine($"Document was analyzed with model version: {result.ApiVersion}");
+            Console.WriteLine($"Document was analyzed and has {result.Pages.Count} pages and {result.Paragraphs.Count} paragraphs.");
+
+
+            foreach (DocumentLanguage language in result.Languages)
+            {
+                Console.WriteLine($"  Found language '{language.Locale}' with confidence {language.Confidence}.");
+            }
         }
 
         /// <summary>
